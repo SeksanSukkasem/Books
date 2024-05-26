f37b422da16310571c075c7dd982997f4f2039ae
SeksanSukkasem <143654703+SeksanSukkasem@users.noreply.github.com>
2024-05-27 01:39:01 +0700

    UPdate Add.ejs

--- app.js
+++ app.js
@@ -3,32 +3,72 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require("path");
+const mysql = require("mysql");
+const multer = require('multer');
 
 const app = express();
-const port = process.env.port || 5000;
+app.use(express.json());
+
+// Set up multer storage for handling file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Specify the destination folder for storing uploads
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname); // Rename the file with a unique name
+    }
+});
+
+const upload = multer({ storage: storage });
+
+// Handle file upload route
+app.post('/upload', upload.single('file'), (req, res) => {
+    // Access uploaded file information using req.file
+    console.log(req.file);
+    res.send('File uploaded successfully!');
+});
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_sql'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL server.');
+});
+
+
+const port = process.env.PORT || 5000;
 const productrouter = require("./src/router/productrouter");
+const { type } = require('os');
 
 app.use(morgan('combined'));
-
+app.use('/', productrouter);
 app.use('/books', productrouter);
-// app.use('/books/:productTitle', productrouter);
+app.use('/addBook', productrouter);
 
-app.use(express.static(path.join(__dirname, "./public/")))
+
+app.use(express.static(path.join(__dirname, "./public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.get('/books/:title', (req, res) => {
-    (req.params.title);
+    const title = req.params.title;
     // Now use the decoded title to fetch the corresponding book or render the page
     // Example:
-    res.render('booksDetail');
+    res.render('booksDetail', { title });
 });
-
 
 app.listen(port, async () => {
     const chalk = await import('chalk');
-    debug("Listening on port" + chalk.default.green(port));
+    debug("Listening on port " + chalk.default.green(port));
 });
 
-
-module.exports = productrouter;
+module.exports = app;
--- src/router/productrouter.js
+++ src/router/productrouter.js
@@ -2,25 +2,65 @@ const express = require('express');
 const mysql = require('mysql');
 const debug = require('debug')('app');
 const productRouters = express.Router();
-const dataBooks = require('../data/dataBooks.json');
 
+// Create MySQL connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_sql'
+});
 
+// Route to get all product books from MySQL database and display only title and description in "/"
 productRouters.get("/", (req, res) => {
-    res.render("booksDetail", {
-        dataBooks
+    const sql = "SELECT productTitle, productDescription FROM products"; // Assuming your table name is 'products'
+    connection.query(sql, (err, data) => {
+        if (err) {
+            debug(err);
+            return res.json("Error");
+        }
+        res.render("product", {
+            product: data // Pass the fetched data to the template
+        });
     });
 });
 
-// // Route to get all product books from MySQL database
-// productRouters.get("/productbooks", (req, res) => {
-//     const sql = "SELECT * FROM productbooks";
-//     con.query(sql, (err, data) => {
-//         if (err) {
-//             debug(err);
-//             return res.json("Error");
-//         }
-//         return res.json(data);
-//     });
-// });
+// Route to get all product books from MySQL database and display all columns in "/books"
+productRouters.get("/books", (req, res) => {
+    const sql = "SELECT * FROM products"; // Assuming your table name is 'products'
+    connection.query(sql, (err, data) => {
+        if (err) {
+            debug(err);
+            return res.json("Error");
+        }
+        res.render("booksDetail", {
+            dataBooks: data // Pass the fetched data to the template
+        });
+    });
+});
+
+productRouters.get('/addBook', (req, res) => {
+    
+    res.render("Add");
+});
+
+
+
+productRouters.post('/addBook', (req, res) => {
+    const { productTitle, productDescription, productPrice, imageUrl } = req.body;
+
+    const query = "INSERT INTO products (productTitle, productDescription, productPrice, imageUrl) VALUES (?, ?, ?, ?)";
+
+    connection.query(query, [productTitle, productDescription, productPrice, imageUrl], (err, data) => {
+        if (err) {
+            debug(err);
+            return res.status(500).json({ error: "Error adding book" });
+        }
+        // Assuming you have an "addBook.ejs" template
+        res.render("Add", {
+            data: data // Pass the fetched data to the template
+        });
+    });
+});
 
 module.exports = productRouters;
